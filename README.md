c983a1123b2241976f28a357933306949f463917
Várnai Gellért <gellert.varnai@webandmore.hu>
2023-08-11 01:30:07 +0200

    a2dp sink test, button etc

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Webradio / A2DP sink using ESP32
 
 ESP32-A2DP as submodule, because it wasn't published to platformio library registry.
+
+#### PCM5102 as DAC, connection:
+VIN -> 5V  
+GND -> GND  
+LCK -> ws_io_num (25)  
+DIN -> data_out_num (17)  
+BCK -> bck_io_num (26)  
+SCK -> GND  
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,18 @@
 #include <WifiConfig.hpp>
 #include <SerialHandler.hpp>
 #include <OneButton.h>
+#include "BluetoothA2DPSink.h"
 #include "secrets.h"
 
-bool debug = false;
+bool debug = true;
 WifiConfig wifiConfig(WIFI_SSID, WIFI_PASSWORD, "ESP32 Kitchen-Radio", "kitchen-radio", AUTH_USER, AUTH_PASS, true, true, debug);
+Configuration outputs("/outputs", debug);
+BluetoothA2DPSink a2dp_sink;
 
 void serialCb(String);
+
+bool btMode = false;
+OneButton modeSw(GPIO_NUM_5);
 
 void setup() {
   if (debug) {
@@ -15,11 +21,38 @@ void setup() {
     delay(10);
   }
 
+  i2s_pin_config_t my_pin_config = {
+    .bck_io_num = 26,
+    .ws_io_num = 25,
+    .data_out_num = 17,
+    .data_in_num = I2S_PIN_NO_CHANGE
+  };
+  a2dp_sink.set_pin_config(my_pin_config);
+
+  modeSw.attachClick([]() {
+    btMode = !btMode;
+    if (btMode) {
+      if (debug) Serial.println("Switching to BT mode");
+      a2dp_sink.start(wifiConfig.getConfig().get("name")->getValue().c_str());
+    } else {
+      if (debug) Serial.println("Switching to Wifi mode");
+      a2dp_sink.end();
+    }
+  });
+
+  pinMode(GPIO_NUM_13, OUTPUT);
+
+  outputs.add("13", LOW, [](int value) {
+    digitalWrite(GPIO_NUM_13, value);
+  });
+
+  wifiConfig.registerConfigApi(outputs);
   wifiConfig.setup();
 }
 
 void loop() {
   wifiConfig.loop();
+  modeSw.tick();
   handleSerial(debug, serialCb);
   delay(1);
 }
